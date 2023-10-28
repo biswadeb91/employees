5eeadf1dc8422c39447e9f595c978e91d28cb5d5
Biswadeb <Biswadeb@LAPTOP-4V6R1FH9>
2023-10-28 15:57:02 +0530

    Salary calculation

---
+++ src/main/java/com/imaginnovate/employees/exception/WrongDateOfJoining.java
@@ -0,0 +1,15 @@
+package com.imaginnovate.employees.exception;
+
+public class WrongDateOfJoining extends Exception {
+
+	public WrongDateOfJoining(String errorMessage) {
+		super(errorMessage);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+}
--- src/main/java/com/imaginnovate/employees/taxcalculation/EmployeeTaxCalculator.java
+++ src/main/java/com/imaginnovate/employees/taxcalculation/EmployeeTaxCalculator.java
@@ -2,38 +2,81 @@ package com.imaginnovate.employees.taxcalculation;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Optional;
 
-import org.hibernate.internal.util.compare.CalendarComparator;
-
+import com.imaginnovate.employees.exception.WrongDateOfJoining;
 import com.imaginnovate.employees.model.Employee;
 
 public class EmployeeTaxCalculator {//Assuming tax calculation for running year
 
-	public Double calculateTax(Employee emp) {
+	public Double calculateTax(Employee emp) throws WrongDateOfJoining {
 		
 		Double salary = emp.getSalary();
 		
-		Double taxableSlary = 0.0;
-		
 		Date doj = emp.getDoj();
 		
+		Double taxableSlary = calculateTaxableSalary(salary, doj);
+		
+		TaxCalculationEngine taxEngine = new TaxCalculationEngine();
+		return taxEngine.calculate(taxableSlary);
+	}
+
+		
+	private Double calculateTaxableSalary(Double salary, Date doj) throws WrongDateOfJoining {
+
+		if(Calendar.getInstance().getTime().before(doj)) {
+			throw new WrongDateOfJoining("Wrong Date of joining. Contact administrator to update.");
+		}
+		Double taxableSlary = 0.0;
 		Date currentdate = new Date();
-		
-		Calendar cal = Calendar.getInstance();
-		
-		if(currentdate.after(doj)) {
-			cal.set(1, 2, Calendar.YEAR);
-			if (doj.before(cal.getTime())) {
+
+		Calendar startCal = Calendar.getInstance();
+		startCal.set(Calendar.DATE, 1);
+		Calendar endCal = Calendar.getInstance();
+
+		//Looping for Jan to Mar this year
+		for(int month = 0; month<12; month++) {
+			startCal.set(Calendar.MONTH, month);
+			endCal.set(Calendar.MONTH, month);
+			endCal.set(Calendar.DATE, endCal.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+
+			if (doj.before(startCal.getTime())) {
 				taxableSlary = salary;
-			} else if (doj.before(cal.add(Calendar.DAY_OF_MONTH, 30).getTime())) {
-				taxableSlary = salary;
+			} else if (doj.before(endCal.getTime())) {
+				taxableSlary = taxableSlary + calculateSalary(doj, startCal, endCal , salary);
 			}
 		}
-		return null;
-		
-		
-	}
+
+
+		startCal.add(Calendar.YEAR, -1);
+		endCal.add(Calendar.YEAR, -1);
+		//Looping for April to DEC
+		for(int month = 3; month<12; month++) {
+			startCal.set(Calendar.MONTH, month);
+			endCal.set(Calendar.MONTH, month);
+			endCal.set(Calendar.DATE, endCal.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+
+			if (doj.before(startCal.getTime())) {
+				taxableSlary = salary;
+			} else if (doj.before(endCal.getTime())) {
+				taxableSlary = taxableSlary + calculateSalary(doj,startCal,  endCal , salary);
+			}
+		}
+
+		return taxableSlary;
+}
+
+
+	private Double calculateSalary(Date doj, Calendar startCal, Calendar endCal, Double salary) {
+		 long diff = endCal.getTime().getTime() - doj.getTime();
+	     long workeddays = (diff / 1000 / 60 / 60 / 24)+1;
+	     
+	     int workingDaysInMonth = endCal.getActualMaximum(Calendar.DAY_OF_MONTH);
+	     Double perdaySalary = salary/workingDaysInMonth;
+	     
+	return perdaySalary*workeddays;
+}
 	
 	
 
